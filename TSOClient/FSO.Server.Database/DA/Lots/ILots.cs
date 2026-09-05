e5f52caaf0a8d9aecba52678281255b840af69dc
Dylan <deadhead3492@gmail.com>
2026-09-05 09:38:37 -0400

    Create lot list rather than single object

--- TSOClient/FSO.Server.Database/DA/Lots/ILots.cs
+++ TSOClient/FSO.Server.Database/DA/Lots/ILots.cs
@@ -14,7 +14,7 @@ namespace FSO.Server.Database.DA.Lots
         DbLot GetByName(int shard_id, string name);
         DbLot GetByLocation(int shard_id, uint location);
         List<DbLot> GetAdjToLocation(int shard_id, uint location);
-        DbLot GetByOwner(uint owner_id);
+        List<DbLot> GetByOwner(uint owner_id);
         DbLot Get(int id);
         List<DbLot> GetMultiple(int[] ids);
         List<DbLot> Get(IEnumerable<int> ids);

c7611e837fdb01d54d1eb1ad2f41bad93a70a9c3
Dylan <deadhead3492@gmail.com>
2026-09-05 17:00:35 -0400

    Maybe

--- TSOClient/tso.client/UI/Panels/UIPersonPage.cs
+++ TSOClient/tso.client/UI/Panels/UIPersonPage.cs
@@ -1133,7 +1133,7 @@ namespace FSO.Client.UI.Panels
                 Buttons = new UIAlertButton[] {
                         new UIAlertButton(UIAlertButtonType.Yes, (btn) => {
                             if (CurrentAvatar.Value != null) {
-			    	FindController<PersonPageController>().ChangeRoommate(ChangeRoommateType.KICK, CurrentAvatar.Value.Avatar_Id, FindController<PersonPageController>()?.LotLocation ?? MyAvatar.Value.Avatar_LotGridXY);
+			    FindController<PersonPageController>().ChangeRoommate(ChangeRoommateType.KICK, CurrentAvatar.Value.Avatar_Id, GameFacade.Scenes.City?.SelectedLotLocation ?? MyAvatar.Value.Avatar_LotGridXY);
                             }
                             UIScreen.RemoveDialog(alert);
                             }),
@@ -1152,7 +1152,7 @@ namespace FSO.Client.UI.Panels
                 Buttons = new UIAlertButton[] {
                         new UIAlertButton(UIAlertButtonType.Yes, (btn) => {
                             if (CurrentAvatar.Value != null) {
-			    	FindController<PersonPageController>().ChangeRoommate(ChangeRoommateType.INVITE, CurrentAvatar.Value.Avatar_Id, FindController<PersonPageController>()?.LotLocation ?? MyAvatar.Value.Avatar_LotGridXY);
+			    FindController<PersonPageController>().ChangeRoommate(ChangeRoommateType.INVITE, CurrentAvatar.Value.Avatar_Id, MyAvatar.Value?.Avatar_LotGridXY ?? 0);
                             }
                             UIScreen.RemoveDialog(alert);
                             }),

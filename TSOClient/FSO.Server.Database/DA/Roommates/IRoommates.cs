5e8cd637ec2bf58d50814841cef8268f1cac7298
Dylan <deadhead3492@gmail.com>
2026-09-06 10:19:25 -0400

    Add DemotePrimaryRoommate

--- TSOClient/FSO.Server.Database/DA/Roommates/IRoommates.cs
+++ TSOClient/FSO.Server.Database/DA/Roommates/IRoommates.cs
@@ -5,6 +5,7 @@ namespace FSO.Server.Database.DA.Roommates
     public interface IRoommates
     {
         bool Create(DbRoommate roomie);
+	bool DemotePrimaryRoommate(uint avatarId);
         bool CreateOrUpdate(DbRoommate roomie);
         DbRoommate Get(uint avatar_id, int lot_id);
         List<DbRoommate> GetAvatarsLots(uint avatar_id);
